849c8b183c5d45fb861ef8008925567de684cd57
Jamie <jamiejahnio@gmail.com>
2025-03-14 08:35:29 +0100

    batman

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FooterComponent} from '../components/footer/footer.component';
+import {HeaderComponent} from '../components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'novaverta';
+  title = 'Novaverta';
 }
